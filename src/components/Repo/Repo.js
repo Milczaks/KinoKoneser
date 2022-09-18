fbdbcf40b2fca1677481a12d2df42c34ccef0686
Milczaks <80547580+Milczaks@users.noreply.github.com>
2022-09-18 15:18:00 +0200

    start page

--- src/components/Repo/Repo.js
+++ src/components/Repo/Repo.js
@@ -1,12 +1,78 @@
 import React from "react";
+import API_URL from "../../utilities/Constants";
+import { useEffect, useState, useCallback } from "react";
 
 function Repo() {
+  const [loadedMovies, setLoadedMovies] = useState([]);
+  const fetchMovieHandler = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/movies/all`);
+
+      if (!response.ok) {
+        throw new Error("Nie udało się pobrać kategorii.");
+      }
+
+      const responseData = await response.json();
+      setLoadedMovies(responseData);
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
+
+  console.log(loadedMovies);
+
   return (
     <div className="font3 mg">
       <h2>Repertuar</h2>
-      <p  className="font center"> W naszym kinie cenimy dobre gusta naszyk klientów oraz dbamy o nich podczas doboru repertuaru.</p>
-      <p  className="font center">U nas zobaczysz takie wspaniałe filmy, jak:</p>
+      <p className="font center">
+        W naszym kinie cenimy dobre gusta naszyk klientów oraz dbamy o nich
+        podczas doboru repertuaru.
+      </p>
+      <p className="font center">U nas zobaczysz takie wspaniałe filmy, jak:</p>
+      <div className="cent">
+        <div className="font2">
+          <div className="right-s">
+            <p>Nazwa filmu:</p>
+            <p className="font3 no-space center important">
+              {loadedMovies.title}
+            </p>
+            <p>Data produkcji:</p>
+            <p className="font center">{loadedMovies.year}</p>
+          </div>
+          <img
+            className="left-s little"
+            src={loadedMovies.posterURL}
+            alt={loadedMovies.title}
+          ></img>
+        </div>
+      </div>
+
+      <div className="cent">
+        <div className="font2">
+          <div className="right-s">
+            <p>Nazwa filmu:</p>
+            <p className="font3 no-space center important">
+              {loadedMovies.title}
+            </p>
+            <p>Data produkcji:</p>
+            <p className="font center">{loadedMovies.year}</p>
+          </div>
+          <img
+            className="left-s little"
+            src={loadedMovies.posterURL}
+            alt={loadedMovies.title}
+          ></img>
+        </div>
+      </div>
     </div>
+    
+
+
+    
   );
 }
 
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,38 +1,54 @@
 //import React, { useState, useEffect } from "react";
-import API_URL from '../../utilities/Constants';
-import { useEffect, useState, useCallback } from 'react'
+import API_URL from "../../utilities/Constants";
+import { useEffect, useState, useCallback } from "react";
 
+// import { useParams } from "react-router-dom";
+
+// const params = useParams();
+// const { orderId } = params;
 
 const Start = () => {
-  // const [showings, setShowings] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("/api/showings/all").then((res) => console.log(res));
-  // }, []);
-
-
-  const [loadedMovies, setLoadedMovies] = useState([]);
-  const fetchMovieHandler = useCallback(async () => {
+  const [loadedMovie4, setLoadedMovie4] = useState([]);
+  const [loadedMovie8, setLoadedMovie8] = useState([]);
+  const fetchMovie4Handler = useCallback(async () => {
     try {
-      const response = await fetch(`${API_URL}/movies/all`);
-      
+      const response = await fetch(`${API_URL}/movies/4`);
+
       if (!response.ok) {
-        throw new Error('Nie udało się pobrać kategorii.');
+        throw new Error("Nie udało się pobrać kategorii.");
       }
 
       const responseData = await response.json();
-      setLoadedMovies(responseData);
+      setLoadedMovie4(responseData);
     } catch (error) {
       alert(error.message);
     }
   }, []);
 
   useEffect(() => {
-    fetchMovieHandler();
-  }, [fetchMovieHandler]);
+    fetchMovie4Handler();
+  }, [fetchMovie4Handler]);
+  
+  const fetchMovie8Handler = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/movies/8`);
 
-  console.log(loadedMovies)
+      if (!response.ok) {
+        throw new Error("Nie udało się pobrać kategorii.");
+      }
 
+      const responseData = await response.json();
+      setLoadedMovie8(responseData);
+    } catch (error) {
+      alert(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMovie8Handler();
+  }, [fetchMovie8Handler]);
+
+  console.log(loadedMovie4);
 
   return (
     <div>
@@ -42,11 +58,44 @@ const Start = () => {
       <div className="font2">
         <p className="font3 space">POLECANE FILMY</p>
         <p className="center space">
-          Na dzień dzisiejszy, polecamy wszystkim naszym gościom najnowsze
-          premiery filmowe:
+          Na dzień dzisiejszy, polecamy wszystkim naszym gościom
+          dwie najpopularniejsze i najchętniej wyszukiwane seanse:
+          <br />
+          <br />
         </p>
-
-        
+        <div>
+          <div className="right-s">
+            <p>Nazwa filmu:</p>
+            <p className="font3 no-space center important">
+              {loadedMovie4.title}
+            </p>
+            <p>Opis:</p>
+            <p className="font center">{loadedMovie4.description}</p>
+            <p>Data produkcji:</p>
+            <p className="font center">{loadedMovie4.year}</p>
+          </div>
+          <img
+            className="left-s little"
+            src={loadedMovie4.posterURL}
+            alt={loadedMovie4.title}
+          ></img>
+          
+          <div className="right-s">
+            <p>Nazwa filmu:</p>
+            <p className="font3 no-space center important">
+              {loadedMovie8.title}
+            </p>
+            <p>Opis:</p>
+            <p className="font center">{loadedMovie8.description}</p>
+            <p>Data produkcji:</p>
+            <p className="font center">{loadedMovie8.year}</p>
+          </div>
+          <img
+            className="left-s little"
+            src={loadedMovie8.posterURL}
+            alt={loadedMovie8.title}
+          ></img>
+        </div>
       </div>
     </div>
   );
